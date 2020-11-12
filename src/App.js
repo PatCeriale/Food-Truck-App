6720f265b62b6c72477f5a444ac5a4432dfc6216
Patrick Ceriale <patceriale@gmail.com>
2020-11-12 14:34:50 -0800

    foot almost done needs to stay at the bottom of an empty page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import React from "react";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 import SignIn from "./pages/signIn/signIn";
 
 function App() {
   return (
-    <>
+    <div className="page-container">
       <Router>
-        <Navbar />
-        <Switch>
-          <Route exact path={["/signin"]}>
-            <SignIn />
-          </Route>
-        </Switch>
-        <Footer />
+        <div className="content-wrap">
+          <Navbar />
+          <Switch>
+            <Route exact path={["/signin"]}>
+              <SignIn />
+            </Route>
+          </Switch>
+          <Footer class="footer-container" />
+        </div>
       </Router>
-    </>
+    </div>
   );
 }
 
