15499a33b1a039c23a6599ae8fa6b93cff946fb6
Josh Brown <joshbrown2794@gmail.com>
2020-11-13 11:41:41 -0800

    gps

--- src/components/GMap/GMap.js
+++ src/components/GMap/GMap.js
@@ -14,6 +14,21 @@ export class MapContainer extends Component {
     activeMarker: {}, // Shows the active marker upon click
     selectedPlace: {}, // Shows the InfoWindow to the selected place upon a marker
   };
+  onMarkerClick = (props, marker, e) =>
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true,
+    });
+
+  onClose = (props) => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null,
+      });
+    }
+  };
   render() {
     return (
       <Map
@@ -24,7 +39,21 @@ export class MapContainer extends Component {
           lat: -1.2884,
           lng: 36.8233,
         }}
-      />
+      >
+        <Marker
+          onClick={this.onMarkerClick}
+          name={"Kenyatta International Convention Centre"}
+        />
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}
+          onClose={this.onClose}
+        >
+          <div>
+            <h4>{this.state.selectedPlace.name}</h4>
+          </div>
+        </InfoWindow>
+      </Map>
     );
   }
 }
