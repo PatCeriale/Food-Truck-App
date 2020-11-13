682143bcd1629fe26756e0fa3a306665cd656df9
Josh Brown <joshbrown2794@gmail.com>
2020-11-13 10:13:20 -0800

    home page skeleton

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import SignIn from "./pages/signIn/signIn";
-
+import Home from "./pages/Home/Home";
 function App() {
   return (
     <>
       <Router>
         <Navbar />
         <Switch>
+          <Route exact path={["/Home"]}>
+            <Home />
+          </Route>
           <Route exact path={["/signin"]}>
             <SignIn />
           </Route>
