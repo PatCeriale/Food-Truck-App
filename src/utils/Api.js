94b2e670809f4fba3b51057f5f44ca1fb2f6fe76
Code Boy <stefanjohnson650@gmail.com>
2020-11-17 18:21:10 -0800

    added updates to sign in

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -3,9 +3,11 @@ import axios from 'axios'
 const API = {
 
 createNewUSer: function (userData){
-return axios.post("http://localhost:5000/newuser", userData)
+return axios.post("http://localhost:3000/newuser", userData)
+},
+createSignIn: function (userData){
+    return axios.post("http:/localhost:3000/signin", userData)
 }
-
 
 }
 
