2825f19bf65d7199f67ee0002ecd026e379b15de
Patrick Ceriale <patceriale@gmail.com>
2020-11-19 18:07:15 -0800

    search trucks page calls API and renders, need to get individual truck page working

--- src/components/TruckResults/TruckResults.js
+++ src/components/TruckResults/TruckResults.js
@@ -1,15 +1,26 @@
 import { Container, IconButton, Button, Grid } from "@material-ui/core";
+import { Link } from "react-router-dom";
+import "./TruckResults.css";
 
-export default function TruckResults(props) {
+export default function TruckResults({
+  googleId,
+  name,
+  rating,
+  address,
+  isOpen,
+}) {
   return (
-    <Container container spacing={6}>
+    <Container container spacing={6} className="TruckResults">
+      <h3>{name}</h3>
       <Grid item xs={12}>
-        <img src="#" />
-        Truck Name: <br />
-        Truck Location: <br />
+        {/* <img src="#" /> */}
+        Truck Location: {address}
+        <br />
         Website: <br />
-        Is open: <br />
-        Global Rating: <br />
+        Is open: {isOpen} <br />
+        Rating: {rating}
+        <br />
+        <Link to={"/truck?id=" + googleId}>more info...</Link>
       </Grid>
     </Container>
   );
