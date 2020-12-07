c0b9e2042d1dce4648c5e1d06990358af14527ea
Patrick Ceriale <patceriale@gmail.com>
2020-12-07 12:29:07 -0800

    reviews are appending to page, need to polish

--- src/components/Rating/Rating.js
+++ src/components/Rating/Rating.js
@@ -8,61 +8,47 @@ import {
 } from "../../utils/Api";
 import { useState, useEffect } from "react";
 
-export default function Review({ vendorId }) {
+export default function Rating({ vendorId }) {
   const [ratingData, setRatingData] = useState([]);
-
-  // useEffect(() => {
-  //   if (!vendorId) return;
-  //   const res = getPlacesTrucks(vendorId);
-  //   // .then((res) =>
-  //   setReviewData(res.data.result.reviews.map());
-  //   // ({ author_name, rating, text, relative_time_description }) => ({
-  //   //   author_name,
-  //   //   rating,
-  //   //   text,
-  //   //   relative_time_description,
-  //   // })
-  //   // );
-  //   // setLoading(false);
-  // }, [vendorId]);
 
   ////////////////////////////////////////////////////////
   useEffect(() => {
-    if (!vendorId) return;
+    // if (!vendorId) return;
     // console.log(vendorId);
-    getReviews(vendorId).then((res) => setRatingData(res));
+    getReviews(vendorId).then((res) => {
+      setRatingData(res.data);
+    });
     // setLoading(false);
     // console.log(ratingData, "+++++++++++++++++++++++++");
   }, [vendorId]);
   /////////////////////////////////////////////////////////
 
   useEffect(() => {
-    console.log(ratingData, "+++++++++++++ Ratings!!!! ++++++++++++");
+    console.log(ratingData, "+++++++++++++ Ratings!!!! ++++++++++++", vendorId);
   }, [ratingData]);
 
   return (
-    //
     <div>
       {ratingData
         .slice(0)
         .reverse()
-        .map((ratings) => (
+        .map((reviews) => (
           <Container className="Container" maxWidth="sm">
             <Grid container spacing={4}>
               <Grid item xs={6} spacing={6}>
-                {/* <h3>{ratings?.author_name}</h3> */}
+                {/* <h3>{reviews?.author_name}</h3> */}
                 <br />
-                {ratings?.reviewCreated}
+                {reviews?.reviewCreated}
               </Grid>
               <Grid item xs={6} spacing={6} className="rating-stars">
-                User Rating: {ratings?.rating}{" "}
+                User Rating: {reviews?.rating}{" "}
                 {/* for loop with length of stars given? */}
                 <i class="far fa-star"></i>
               </Grid>{" "}
               <Grid item xs={12} spacing={12}>
                 <hr />
                 <br />
-                <p>{ratings?.reviewText}</p>
+                <p>{reviews?.reviewText}</p>
               </Grid>
             </Grid>
           </Container>
